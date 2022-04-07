37af59728ab362c1243873a34aa694a11dccff38
MosesYalman <Moses.y@hotmail.com>
2022-04-07 09:06:45 +0200

    few changes

--- Script/SokobanBase.js
+++ Script/SokobanBase.js
@@ -1,25 +1,22 @@
-/*   This is the base file for the Sokoban assignment - include this one in your HTML page, before you add the main script file*/
 
-/*   Enum of CSS Classes for the static elements   */
+
+
+
+
 var Tiles = {
   Wall: "tile-wall",
   Space: "tile-space",
   Goal: "tile-goal",
 };
 
-/*   Enum of CSS Classes for the moving elements   */
+
 var Entities = {
   Character: "entity-player",
   Block: "entity-block",
   BlockDone: "entity-block-goal",
 };
 
-/*  Legend
-    W = Wall
-    B = Movable block
-    P = Player starting position
-    G = Goal area for the blocks
-*/
+
 var tileMap01 = {
   width: 19,
   height: 16,
